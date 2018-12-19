fc68aa864bf25a166fbef373c55b82478961953e
egorandreevich <45241457+egorandreevich@users.noreply.github.com>
2018-12-19 02:12:01 +0000

    Update diplom_API_2.py

--- diplom_API_2.py
+++ diplom_API_2.py
@@ -21,31 +21,30 @@ class User: #Опишем юзера как класс
             # 'count': '500'
         }
         response = requests.get('https://api.vk.com/method/groups.get', params)
-        return response.json()  # на выходе должен быть джсон, записанный в файл
+        return response.json()
 
-
-    def get_friends(self):
+    def get_friends(self): #получаю список друзей юзера
         params = {
             'access_token': self.my_token,
             'v': '5.92',
             'user_id': self.user_id_number,
-            'count' : '5' #пока ограничимся пятью, тормозит нереально
+            # 'count' : '5' #пока ограничимся пятью, тормозит нереально
         }
         response = requests.get('https://api.vk.com/method/friends.get', params)
         return response.json()
 
-    def info_about_group(self, group_id):
+    def info_about_group(self, group_id): #получаю расширенную информацию о группе
         params = {
             'access_token': self.my_token,
             'v': '5.92',
             'group_id': group_id,
             'fields' : 'members_count'
-            'count': '5'  # пока ограничимся пятью, тормозит нереально
+            # 'count': '5'  # пока ограничимся пятью, тормозит нереально
         }
         response = requests.get('https://api.vk.com/method/groups.getById', params)
         return response.json()
 
-    def make_class_items(self):
+    def make_class_items(self): #функция для превращения айдишек друзей в экземпляры класса
         list_1 = self.friends_list
         list_2 = []
         # print(list_1)
@@ -73,14 +72,14 @@ class User: #Опишем юзера как класс
                 pass
         return response_list
 
-    def find_unique_groups(self):
+    def find_unique_groups(self): #функция для сравнения множеств - на выходе получаю множество с уникальными группами юзера
         set_1 = set(self.groups_get()['response']['items'])
         set_2 = set(self.get_groups_of_friends())
         set_3 = set_1.difference(set_2)
         print('Уникальные группы у указанного юзера: {}'.format(set_3))
         return set_3
 
-    def info_unique_groups(self):
+    def info_unique_groups(self): #получаю расширенную инфу о группах и привожу ее в нужный формат
         list_1 = list(self.find_unique_groups())
         # print(list_1)
         final_json_list = []
@@ -104,7 +103,7 @@ class User: #Опишем юзера как класс
         # pprint(final_json_list)
         return final_json_list
 
-    def save_to_file(self):
+    def save_to_file(self): #записываю в файл результат приведения расширенной инфы о группах в нужный формат
         final_json_list = self.info_unique_groups()
         with open ('groups.json', 'w') as f:
             f.write(json.dumps(final_json_list, ensure_ascii=False))
@@ -112,9 +111,8 @@ class User: #Опишем юзера как класс
 
     my_token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
     user_id_number = 0
-    # user_name = 'eshmargunov'
-
-def get_id(user):
+   
+def get_id(user): #функция вне класса для получения айди первого юзера, напр, если ввели короткую ссылку eshmargunov
     params = {
         'access_token': 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae',
         'v': '5.92',
@@ -127,22 +125,11 @@ def get_id(user):
             if key == 'id':
                 id = value
         return id
-
-
+    
 
 if __name__ == "__main__":
     user = get_id(input('Сейчас будем искать уникальные группы пользователя.\nНапишите короткую ссылку или айди страницы, которую будем исследовать:\n'))
     evgeniy = User(user)
-# evgeniy = User(40635795)
-# pprint(evgeniy.friends_list)
-# pprint(evgeniy.groups_get())
-# set_1 = set(evgeniy.groups_get()['response']['items'])
-# set_2 = set(evgeniy.get_groups_of_friends())
-# pprint(set_2)
-# print('Уникальные группы у указанного юзера: {}'.format(set_1.difference(set_2)))
-# evgeniy.find_unique_groups()
-# evgeniy.info_unique_groups()
-# evgeniy.info_about_group()
     evgeniy.save_to_file()
 
 
